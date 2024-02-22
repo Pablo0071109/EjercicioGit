e951b873c37016773bed38045f0bab8d7f0b2b5a
Pablo0071109 <ppurasmunoz13@gmail.com>
2024-02-22 09:43:43 +0100

    Lineas del metodo main

--- src/ejerciciogit/EjercicioGit.java
+++ src/ejerciciogit/EjercicioGit.java
@@ -14,7 +14,8 @@ public class EjercicioGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Soy pablo");
+        System.out.println("ASTon martin");
     }
     
 }
